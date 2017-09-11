d7b0b00a9cfd352f95dd9253360373e289826b5d
Fábio L. Fischer <feuture@hotmail.com>
2017-09-11 14:29:17 -0300

    trabalho-02 questao-02

---
+++ trabalhos/trabalho-02/questao-02/src/App.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ *   FURB - Bacharelado em Ciências da Computação
+ *   Teoria dos Grafos
+ *   Trabalho 02 - Questão 01
+ *
+ *   Fábio Luiz Fischer
+ *
+ *     Dada a matriz de adjacência do grafo de ordem n, você deve implementar um programa que responda as seguintes perguntas:
+ *          a. Qual é o tipo do grafo (dirigido ou não, simples ou multigrafo, regular, completo, nulo ou bipartido)
+ *              nome do método: tipoDoGrafo
+ *              parâmetro de entrada: matriz de adjacência
+ *              retorno: String contendo o tipo do grafo
+ *
+ *          b.Quantas arestas esse grafo possui? Liste o conjunto de arestas.
+ *              nome do método: arestasDoGrafo
+ *              parâmetro de entrada: matriz de adjacência
+ *              retorno: String com a quantidade e o conjunto de arestas
+ *
+ *          c.Qual é o grau de cada vértice. Liste a sequência de graus.
+ *              nome do método: grausDoVertice
+ *              parâmetro de entrada: matriz de adjacência
+ *              retorno: String identificando o grau de cada vértice e por fim, a sequência de graus
+ *
+ **/
+
+public class App {
+
+    public static void main(String[] args) {
+        int[][] matrizAdj = {
+                {0, 3, 0, 0},
+                {9, 0, 7, 0},
+                {0, 0, 4, 2},
+                {0, 0, 1, 0}};
+
+        Grafo g = new Grafo(matrizAdj);
+
+        System.out.println(g.toString());
+    }
+}
